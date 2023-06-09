ed7411c8eb261079c56b8147b9b632b781637eb6
MargoStranger <margosebastian13@gmail.com>
2023-06-09 18:52:42 +0300

    условия

---
+++ Example005_Favorit/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Давай знакомиться! Введи свое имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марго")
+{
+    Console.Write("Ура, это же Марго!!!"); 
+}
+else
+{
+    Console.Write("Привет,  "); 
+    Console.Write(username);
+}
+
+
+
